bc51628c36e8661bc68b0fa373645337de536be2
Jone Bakkevig <jonekb13@student.uia.no>
2017-07-14 11:52:03 +0200

    added pca implementation and plots

--- excelparse.py
+++ excelparse.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.ticker as tck
-import seaborn as sns
-from matplotlib.font_manager import FontProperties
 
 __author__ = 'j.k.bakkevig@gmail.com'
 
--- pca.py
+++ pca.py
@@ -5,39 +5,83 @@ from sklearn.decomposition import PCA as sklearnPCA
 import sklearn.preprocessing as skp
 from excelparse import fileToDframe
 
-df = fileToDframe('Data_nieuwveer.xlsx', 92, [4,6,42,79], 1)
-n = df.values
+
+columns = [3, 4, 7, 15, 35, 36, 43, 66, 79]
+columns_ = [3, 23, 25]
+
+
+df1 = fileToDframe('Data_nieuwveer.xlsx', 92, columns, 1)
+df2 = fileToDframe('Data_nieuwveer.xlsx', 2, columns_, 3)
+
+
+# Concatenating the frames from the separately parsed sheets
+df = pd.concat([df1, df2], axis=1)
+
+# Dropping NaN values
+clean_df = df.dropna(subset=['COD','COD.1','COD.2','Precipitation','SRT'])
+
+n = clean_df.values
+
+# Normalize data for variying data sizes.
 min_max_scaler = skp.MinMaxScaler()
 n_scaled = min_max_scaler.fit_transform(n)
 
-imp = skp.Imputer(missing_values='NaN', strategy='mean', axis=1)
-clean_df = pd.DataFrame(imp.fit_transform(n_scaled),columns=df.columns, index=df.index)
+def findNcomp(data):
+    # run to find optimal number of components. 
+    pca = sklearnPCA()
+    pca.fit(data)
+    # variance ratio as variable
+    var = pca.explained_variance_ratio_
+    # Create graph to determine best number of components
+    plt.figure(figsize=(8, 4))
+    plt.bar(np.arange(1,len(var)+1, 1.0), np.cumsum(var))
+    plt.xticks(np.arange(0, len(var)+1, 1.0))
+    plt.yticks(np.arange(0.1 ,1.1 , 0.1))
+    plt.xlabel('n_components')
+    plt.ylabel('explained_variance_ratio')
+    plt.show()
 
-def doPca(data, n_comp=2):
+def doPca(data, n_comp=None):
+
     pca = sklearnPCA(n_components=n_comp)
     pca.fit(data)
-    # Create graph to determine best component number
+
     if n_comp == None:
-        plt.figure(figsize=(8, 4))
-        plt.plot(pca.explained_variance_)
-        plt.xticks(np.arange(0, 4, 1.0))
-        plt.xlabel('n_components')
-        plt.ylabel('explained_variance_')
-        plt.show()
+        findNcomp(data)
 
     return pca
 
-pca = doPca(clean_df)
+target = clean_df.ix[:,6].values
+
+pca2 = doPca(n_scaled, 2)
+X_1 = pca2.transform(n_scaled)
 
 
+pca3 = doPca(n_scaled, 3)
+X_2 = pca3.transform(n_scaled)
 
+# ax1 = plt.scatter(X_1[:, 0], X_1[:, 1], c=target)
+# plt.colorbar(ax1)
+# plt.xlabel('PC1')
+# plt.ylabel('PC2')
+# plt.show()
+#
+# ax2 = plt.scatter(X_2[:, 1], X_2[:, 2], c=target)
+# plt.colorbar(ax2)
+# plt.xlabel('PC2')
+# plt.ylabel('PC3')
+# plt.show()
+#
+# ax3 = plt.scatter(X_2[:, 0], X_2[:, 2], c=target)
+# plt.colorbar(ax3)
+# plt.xlabel('PC1')
+# plt.ylabel('PC3')
+# plt.show()
 
-# print pca.explained_variance_
-# print pca.explained_variance_ratio_
-
-
-
-
+# plt.bar(np.arange(1, len(pca2.components_)+1, 0.1), pca2.components_[0])
+# plt.bar
+# plt.show()
+print pca2.components_
 
 
 
